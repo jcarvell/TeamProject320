3773d8d00601205135044dd034da1577ce593c67
Ed Reckard <ereckard@ycp.edu>
2022-03-13 20:06:19 -0400

    More changes
    yer

--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,22 @@
-
 public class Map {
-
+	
+	
+	Room[] roomChoices;
+	
+	public Map() {
+		roomChoices[0]=null;
+	} 
+	public void addRoom(Room a) {
+		
+		roomChoices[roomChoices.length]=a;
+		
+	}
+	public Room choiceOne(int roomNumber){
+		
+		return roomChoices[roomNumber];
+	}
+	public Room choiceTwo(int roomNumber){
+		
+		return roomChoices[roomNumber];
+	}
 }
