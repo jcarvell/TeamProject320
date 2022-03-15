45d89b0678f36570b795d2f20e00e853d7a11982
Ed Reckard <ereckard@ycp.edu>
2022-03-14 22:40:30 -0400

    Ed's Changes
    Constructors made for potion, weapon, item
    Weapons are now made in a for loop with enemys for game
    Enemy's now have health, strength, speed, and an option for weapon
    All you need to do to construct a weapon or potion is construct an item with the correct itemType(Check constructor)

--- src/Room.java
+++ src/Room.java
@@ -3,4 +3,15 @@ public class Room {
 
 	private int roomNumber;
 	private String name;
+	
+	public Room(String n, int r) {
+		roomNumber=r;
+		name=n;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getRoomNumber() {
+		return roomNumber;
+	}
 }
