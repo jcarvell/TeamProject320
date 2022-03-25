848ae7d1cb3e38fdb1109edadd3d9a8ee3e149c7
Alina Endres <aendres1@ycp.edu>
2022-03-24 20:30:29 -0400

    Room Resources, NPCs

--- src/RoomResources.java
+++ src/RoomResources.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class RoomResources {
 	private static int maxEnemies = 5, maxNPCs = 5, maxPotions = 1, maxWeapons = 1;		//feel free to change these values
 	
-	private int numEnemies, numNPCs;
+	private int numEnemies, numNPCs, numPotions, numWeapons;
 	
 	private Potion potion;
 	
@@ -15,7 +15,19 @@ public class RoomResources {
 	
 	
 	public RoomResources() {
+		Random rand = new Random(); // instance of random class
+		numEnemies = rand.nextInt(maxEnemies+1);		// set the number of enemies to a random number between zero and maxEnemies
+		numNPCs = rand.nextInt(maxNPCs+1);				// set the number of NPCs to a random number between zero and maxNPCs
+		// this is assuming you can have one potion and one weapon in a room
+		numPotions = rand.nextInt(maxPotions+1);		// set the number of potions to a random number between zero and maxPotions
+		numWeapons = rand.nextInt(maxWeapons+1);		// set the number of weapons to a random number between zero and maxWeapons
 		
+		for(int i = 0; i < numEnemies; i++) {
+			enemyArray[i] = new Enemy();
+		}
+		for(int j = 0; j < numNPCs; j++) {
+			NPCArray[j] = new NPC();
+		}
 	}
 	
 	public Potion getPotion() {
