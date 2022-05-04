0b1987f07ef67d35fc41111f125708a5ba03d792
Alina Endres <aendres1@ycp.edu>
2022-05-04 08:19:11 -0400

    Start test for the RoomResources' random elements

---
+++ JUnitTests/RoomResources_Random.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import teamproject.cs320.Enemy;
+import teamproject.cs320.NPCs;
+import teamproject.cs320.Potion;
+import teamproject.cs320.RoomResources;
+import teamproject.cs320.Weapon;
+
+class RoomResources_Random {
+
+	@Test
+	void test() {
+		int numTests = 100;
+		RoomResources r[] = new RoomResources[numTests];
+		Enemy enemy[] = new Enemy[numTests];
+		
+		for(int i = 0; i < numTests; i++) {
+			r[i] = new RoomResources();
+		}
+		
+		for(int i = 0; i < numTests; i++) {
+			enemy[i] = r[i].getEnemy();
+		}
+	}
+
+}
