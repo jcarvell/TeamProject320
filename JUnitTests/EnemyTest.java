eb5ce546163fc304a1812d394b72060acb33e507
Alina Endres <aendres1@ycp.edu>
2022-05-05 18:45:52 -0400

    Added testSetHealth() to EnemyTest

--- JUnitTests/EnemyTest.java
+++ JUnitTests/EnemyTest.java
@@ -54,7 +54,71 @@ class EnemyTest {
 
 	@Test
 	void testSetHealth() {
-		fail("Not yet implemented");
+		// Declare enemies
+		Enemy monster = new Enemy(5, 7, 9, "Green Monster");
+		Enemy Bob = new Enemy(0, 5, 10, "Bob the Bad Guy");
+		Enemy dragon = new Enemy(10, 10, 0, "Dragon");
+		
+		// Check initial health
+		assertEquals(monster.getHealth(), 5);
+		assertEquals(Bob.getHealth(), 0);
+		assertEquals(dragon.getHealth(), 10);
+		
+		// Use setHealth()
+		monster.setHealth(10);
+		Bob.setHealth(1);
+		dragon.setHealth(5);
+		
+		// Check setHealth() worked
+		assertEquals(monster.getHealth(), 10);
+		assertEquals(Bob.getHealth(), 1);
+		assertEquals(dragon.getHealth(), 5);
+
+		// Use setHealth() for a second time
+		monster.setHealth(0);
+		Bob.setHealth(2);
+		dragon.setHealth(5);
+		
+		// Check setHealth() worked
+		assertEquals(monster.getHealth(), 0);
+		assertEquals(Bob.getHealth(), 2);
+		assertEquals(dragon.getHealth(), 5);
+
+		// Use setHealth() for a third time
+		monster.setHealth(27);
+		Bob.setHealth(42);
+		dragon.setHealth(55);
+		
+		// Check setHealth() worked
+		assertEquals(monster.getHealth(), 27);
+		assertEquals(Bob.getHealth(), 42);
+		assertEquals(dragon.getHealth(), 55);
+		
+		
+		// Declare enemies using Enemy()
+		Enemy snake = new Enemy();
+		Enemy lizard = new Enemy();
+		Enemy newt = new Enemy();
+		
+		// Use setHealth()
+		snake.setHealth(5);
+		lizard.setHealth(33);
+		newt.setHealth(97);
+		
+		// Check setHealth() worked
+		assertEquals(snake.getHealth(), 5);
+		assertEquals(lizard.getHealth(), 33);
+		assertEquals(newt.getHealth(), 97);
+		
+		// Use setHealth() for a second time
+		snake.setHealth(0);
+		lizard.setHealth(30);
+		newt.setHealth(76);
+		
+		// Check setHealth() worked
+		assertEquals(snake.getHealth(), 0);
+		assertEquals(lizard.getHealth(), 30);
+		assertEquals(newt.getHealth(), 76);
 	}
 
 	@Test
